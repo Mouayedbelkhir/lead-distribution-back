7b4f8363a6ceb7c1dedc8fd8b12123fbca3b2817
Belkhir Mouayed <Mouayed@tekand.me>
2026-07-14 13:14:02 +0100

    feat: enforce admin authorization on client mutations

--- src/routes/client.routes.js
+++ src/routes/client.routes.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const { authenticate } = require("../middleware/auth.middleware");
+const { authorize } = require("../middleware/authorize.middleware");
 const clientController = require("../controllers/client.controller");
 
 router.get("/", authenticate, clientController.list);
 router.get("/:id", authenticate, clientController.getById);
-router.post("/", authenticate, clientController.create);
-router.put("/:id", authenticate, clientController.update);
-router.delete("/:id", authenticate, clientController.remove);
+router.post("/", authenticate, authorize("ADMIN"), clientController.create);
+router.put("/:id", authenticate, authorize("ADMIN"), clientController.update);
+router.delete("/:id", authenticate, authorize("ADMIN"), clientController.remove);
 
 module.exports = router;
